af6bfef977ad5d097a2d1a330cc59a580443fc4c
Paul Hanchett <phanchet@jaguarlandrover.com>
2014-03-27 13:28:35 -0700

    Update documentation in comment

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+#
+#   This file initializes the content of the submodules (repositories) referred to by this project.
+#
+#   If you find that _common or build directories are empty, run this file!
+#
 git submodule init
 git submodule update
