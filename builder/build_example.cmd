c100adf6df167455fb362e7c900f3d21ac47c7f6
Paul Hanchett <phanchet@jaguarlandrover.com>
2014-03-27 14:42:48 -0700

    Changed repository to call on common builder repository, com.jlr.pdx-ostc.open.All-IntelPoc-Apps.builder.subModule.git.

--- builder/build_example.cmd
+++
@@ -1,3 +0,0 @@
-set ANT_HOME=c:\tizen-sdk\ide\plugins\org.apache.ant_1.8.2.v20120109-1030
-set JAVA_HOME=c:\Progra~2\Java
-set PATH=%PATH%;%ANT_HOME%\bin
--- builder/build_example.sh
+++
@@ -1,3 +0,0 @@
-export ANT_HOME=./ant
-export JAVA_HOME=/usr/local/jdk1.7.0_51
-export PATH=${PATH}:${ANT_HOME}/bin
